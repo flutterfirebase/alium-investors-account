4a694060f6ddfad168060bc19b965eef6dcde8b7
SurgeryLab <johnaton21@icloud.com>
2021-05-27 20:55:39 +0300

    Fix Build

--- src/pages/InvestorsAccount/components/NftAccountCard/index.tsx
+++ src/pages/InvestorsAccount/components/NftAccountCard/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Input, Text } from '@alium-official/uikit'
+import { Button, Flex } from '@alium-official/uikit'
 import React, { useCallback, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import Modal from '../../../../components/Modal'
